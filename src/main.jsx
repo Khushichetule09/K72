1c24b268faf7d764c5bda92b13016f5e5513ef76
khushichetule09 <khushichetule2@gmail.com>
2025-08-30 12:16:35 +0530

    Fix routing and GSAP setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import Stairs from './components/common/Stairs.jsx'
 import NavContext from './context/NavContext.jsx'
+import { BrowserRouter } from 'react-router-dom'   // <-- yeh line add karo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
